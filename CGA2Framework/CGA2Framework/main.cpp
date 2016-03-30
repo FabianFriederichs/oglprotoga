5f24adff97c2b3697fa79ec8d1f8f89ea59e702e
Fabian Friederichs <fabian.friederichs@gmail.com>
2016-03-31 01:44:07 +0200

    libshit

--- CGA2Framework/CGA2Framework/main.cpp
+++ CGA2Framework/CGA2Framework/main.cpp
@@ -1,6 +1,9 @@
 #include <GL\glew.h>
 #include <glfw3.h>
 #include <glm/glm.hpp>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
 
 
 int main(void)
@@ -8,27 +11,39 @@ int main(void)
 
 	GLFWwindow* window;
 
-	/* Initialize the library */
 	if (!glfwInit())
+	{
+		fprintf(stderr, "Failed to initialize GLFW\n");
+		getchar();
 		return -1;
+	}
 
-	/* Create a windowed mode window and its OpenGL context */
 	glfwWindowHint(GLFW_SAMPLES, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	window = glfwCreateWindow(1000, 800, "Hello World", NULL, NULL);
-	if (!window)
-	{
+	// Open a window and create its OpenGL context
+	window = glfwCreateWindow(1024, 768, "Tutorial 01", NULL, NULL);
+	if (window == NULL){
+		fprintf(stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n");
+		getchar();
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// Initialize GLEW
+	if (glewInit() != GLEW_OK) {
+		fprintf(stderr, "Failed to initialize GLEW\n");
+		getchar();
 		glfwTerminate();
 		return -1;
 	}
 
-	/* Make the window's context current */
-	glfwMakeContextCurrent(window);
-
+	// Ensure we can capture the escape key being pressed below
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
