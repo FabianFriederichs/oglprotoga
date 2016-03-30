5f9370e25bf23e9559968f1bbb0b75d33bc829f7
Futon <seiresleets666@gmail.com>
2016-03-30 22:07:40 +0200

    stuff

--- CGA2Framework/CGA2Framework/GameWindow.h
+++ CGA2Framework/CGA2Framework/GameWindow.h
@@ -1,8 +1,16 @@
+#include <GL\glew.h>
+#include <glfw3.h>
+
 #pragma once
 class GameWindow
 {
 public:
 	GameWindow();
 	~GameWindow();
+
+	bool initialize();
+
+private:
+	GLFWwindow* m_window;
 };
 
