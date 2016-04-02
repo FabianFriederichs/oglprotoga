6bd2f7789aba352029edbc6cfef919bb96062a8e
Futon <seiresleets666@gmail.com>
2016-04-02 22:52:09 +0200

    Weow fam

--- CGA2Framework/CGA2Framework/Scene.h
+++ CGA2Framework/CGA2Framework/Scene.h
@@ -1,4 +1,13 @@
 #pragma once
+
+/*
+	Keep track of Entity-Shader relations (Map with shader ID as key?)
+	Render function to call every Shader's entities
+	
+	Keep track of camera
+
+	#morestufftodo
+*/
 class Scene
 {
 public:
