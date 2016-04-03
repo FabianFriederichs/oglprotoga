cf4194e5f1e6e1ef3d3f3f2cb2027922aee4b5c4
Fabian Friederichs <fabian.friederichs@gmail.com>
2016-04-04 00:09:39 +0200

    changed output variable name in fragment shader

--- CGA2Framework/CGA2Framework/Sample.fs
+++ CGA2Framework/CGA2Framework/Sample.fs
@@ -1,8 +1,8 @@
 #version 330 core
 
-out vec4 gl_FragColor;
+out vec4 outcol;
 
 void main()
 {
-    gl_FragColor = vec4(sin(gl_FragCoord.x), 0.5f, 0.5f, 1);
+    outcol = vec4(sin(gl_FragCoord.x), 0.5f, 0.5f, 1);
 } 
